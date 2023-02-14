3c9cdc3934509ae7c36eaa44119704599b9270d2
DmitryShulzhenko <nikola68sh@yandex.ru>
2023-02-14 15:05:09 +0300

    minor edits in the navbar.module.css file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import classes from './Navbar.module.css';
 
+console.log(classes);
+
 // let classes = {
 //     'nav': 'Navbar_nav__MBUxT',
 //     'item': 'Navbar_item__pvAZV'
+//     'active' : 'Blalala_active'
 // }
+
+//let c1 = "item";
+//let c2 = "active";
+// "item active"
+
+
+//let classes = c1 + " " + c2;
+//let classesNew = `${classes.item} ${classes.active}`;
 
 const Navbar = () => {
     return <nav className={classes.nav}>
         <div className={classes.item}>
             <a>Profile</a>
         </div>
-        <div className={classes.item}>
+        <div className={`${classes.item} ${classes.active}`}>
             <a>Messages</a>
         </div>
         <div className={classes.item}>
